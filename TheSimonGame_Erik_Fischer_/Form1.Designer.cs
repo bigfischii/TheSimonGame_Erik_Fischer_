175ccd6530aacf4e4c7e08c006eaf181da3e93c1
Erik Fischer <0323fier@jonkoping.se>
2022-04-29 12:18:54 +0200

    v.0.2

--- TheSimonGame_Erik_Fischer_/Form1.Designer.cs
+++ TheSimonGame_Erik_Fischer_/Form1.Designer.cs
@@ -31,10 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.Run = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Run
             // 
+            this.Run.Interval = 1000;
             this.Run.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // panel1
@@ -43,17 +48,59 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(400, 400);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(645, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(645, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Highscore:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -61,6 +108,10 @@
 
         private System.Windows.Forms.Timer Run;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TheSimonGame_Erik_Fischer_/Form1.cs
+++ TheSimonGame_Erik_Fischer_/Form1.cs
@@ -13,11 +13,18 @@ namespace TheSimonGame_Erik_Fischer_
     public partial class Form1 : Form
     {
         //Author: Erik Fischer
-        
+        int level = 0;
+        int HS = 0; //Highscore
+        int SR = 0; //Score that game
+        List<int> musx = new List<int>();
+        List<int> musy = new List<int>();
+        Random rnd_simonsq = new Random();
         
         public Form1()
         {
             InitializeComponent();
+            panel1.Invalidate();
+            label2.Text = HS.ToString();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -27,7 +34,103 @@ namespace TheSimonGame_Erik_Fischer_
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //Random square lightup (lvl1 s1 = 1; lvl1 s1 = 2....)
+            if (HS == 12) //score taht game = 12 extend to 3*3
+            {
+                level++;
+            }
+            if (HS == 24) // score that game = 24 extend to 4*4
+            {
+                level++;
+            }
+            panellightup();
+            
+        }
 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush p = new SolidBrush(Color.Black);
+            if (level == 0)
+            {
+                //row 1 column 1;2
+                g.FillRectangle(p, 0, 0, 95, 95);
+                g.FillRectangle(p, 0, 100, 95, 95);
+                //row 2 column 1;2
+                g.FillRectangle(p, 100, 0, 95, 95);
+                g.FillRectangle(p, 100, 100, 95, 95);
+            }
+            if (level == 1)
+            {
+                //row 1 column 1;3
+                g.FillRectangle(p, 0, 0, 95, 95);
+                g.FillRectangle(p, 0, 100, 95, 95);
+                g.FillRectangle(p, 0, 200, 95, 95);
+                //row 2 column 1;3
+                g.FillRectangle(p, 100, 0, 95, 95);
+                g.FillRectangle(p, 100, 100, 95, 95);
+                g.FillRectangle(p, 100, 200, 95, 95);
+                //row 3 column 1;3
+                g.FillRectangle(p, 200, 0, 95, 95);
+                g.FillRectangle(p, 200, 100, 95, 95);
+                g.FillRectangle(p, 200, 200, 95, 95);
+            }
+            if (level == 2)
+            {
+                //row 1 column 1;4
+                g.FillRectangle(p, 0, 0, 95, 95);
+                g.FillRectangle(p, 0, 100, 95, 95);
+                g.FillRectangle(p, 0, 200, 95, 95);
+                g.FillRectangle(p, 0, 300, 95, 95);
+                //row 2 column 1;4
+                g.FillRectangle(p, 100, 0, 95, 95);
+                g.FillRectangle(p, 100, 100, 95, 95);
+                g.FillRectangle(p, 100, 200, 95, 95);
+                g.FillRectangle(p, 100, 300, 95, 95);
+                //row 3 column 1;4
+                g.FillRectangle(p, 200, 0, 95, 95);
+                g.FillRectangle(p, 200, 100, 95, 95);
+                g.FillRectangle(p, 200, 200, 95, 95);
+                g.FillRectangle(p, 200, 300, 95, 95);
+                //row 4 column 1;4
+                g.FillRectangle(p, 300, 0, 95, 95);
+                g.FillRectangle(p, 300, 100, 95, 95);
+                g.FillRectangle(p, 300, 200, 95, 95);
+                g.FillRectangle(p, 300, 300, 95, 95);
+            }
+            
+
+        }
+
+        private void lvl2()
+        {
+
+        }
+
+        private void PanelClaear()
+        {
+            panel1.Invalidate();
+        }
+        
+        private void panellightup()
+        {
+            for (int i = 0; i < SR + 1 ; i++)
+            {
+                if (level == 0)
+                {
+
+                }
+                else if (level == 1)
+                {
+
+                }
+                else
+                {
+
+                }
+
+                
+            }
         }
     }
 }
